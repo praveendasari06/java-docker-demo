86988bbec236378094e2aed5b92d9187049e347a
Cloud User <ec2-user@ip-172-31-10-155.ap-south-1.compute.internal>
2025-05-27 08:56:24 +0000

    test folder along with test file added. build.sh updated

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mkdir -p out
+mkdir -p out test_out
+
+# Compile Main program
 javac Main.java -d out
+
+# Compile tests (make sure junit is available)
+javac -cp .:junit-4.13.2.jar:hamcrest-core-1.3.jar:test test/MainTest.java -d test_out
 
---
+++ test/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMainOutput() {
+        assertEquals("Hello from Dockerized Java App!", "Hello from Dockerized Java App!");
+    }
+}
+
